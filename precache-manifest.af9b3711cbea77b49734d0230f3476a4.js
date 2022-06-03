d14853afa89cb9dc123578715d22fef2a5b782b7
Sam Smith <korhan.tokgoz92@gmail.com>
2022-06-03 22:47:02 +0300

    Updates

---
+++ precache-manifest.af9b3711cbea77b49734d0230f3476a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16536338d9428306a2cd09a9807e32b3",
+    "url": "/accordion//index.html"
+  },
+  {
+    "revision": "afc94dbefb61462d11cf",
+    "url": "/accordion//static/css/main.071378ba.chunk.css"
+  },
+  {
+    "revision": "ab85a117f39d501e282e",
+    "url": "/accordion//static/js/2.40b1d22a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/accordion//static/js/2.40b1d22a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afc94dbefb61462d11cf",
+    "url": "/accordion//static/js/main.608b28f2.chunk.js"
+  },
+  {
+    "revision": "c2a3351afd338ecedac2",
+    "url": "/accordion//static/js/runtime-main.7da872ab.js"
+  }
+]);
